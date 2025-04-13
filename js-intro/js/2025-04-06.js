0a9527584ff427ada832a54c1f62250e5b98bbde
chiz0oo <zokyoudzokyoud@gmail.com>
2025-04-13 15:29:40 +0300

    caca

--- js-intro/js/2025-04-06.js
+++ js-intro/js/2025-04-06.js
@@ -1,32 +1,50 @@
 // + - * /
-let sis1, sis2, dick, result = NaN;
 
-sis1 = Number(prompt("введите первое число.!!!!!", "0"));
-sis2 = Number(prompt("введите второе число.!!!!!", "0"));
-dick = prompt("введите действие (= - * /).!!!!!", "+");
+function calculate(dick, sis1, sis2) {
+    let result = NaN
+    switch (dick) {
+        case "+":
+            result = sis1 + sis2
+            break;
 
-switch (dick) {
-    case "+":
-        result = sis1 + sis2
-        break;
+        case "-":
+            result = sis1 - sis2
+            break;
 
-    case "-":
-        result = sis1 - sis2
-        break;
+        case "*":
+            result = sis1 * sis2
+            break;
 
-    case "*":
-        result = sis1 * sis2
-        break;
+        case "/":
+            if (0 != sis2) result = sis1 / sis2;
+            break;
 
-    case "/":
-        result = sis1 / sis2
-        break;
+        
+    }
+    return result;
+} 
 
+function calculator() {
     
-}
 
-if(isNaN(result)) {
-    alert("ты еблан?")
-} else {
-    alert(`ответ: ${result}`)
+
+    let sis1, sis2, dick, result = NaN;
+
+
+    while (confirm("запустить калькулятор?")) {
+
+        sis1 = Number(prompt("введите первое число.!!!!!", "0"));
+        sis2 = Number(prompt("введите второе число.!!!!!", "0"));
+        dick = prompt("введите действие (= - * /).!!!!!", "+");
+
+        result = calculate(dick, sis1, sis2)
+
+        if(isNaN(result)) {
+            alert("ты еблан?")
+        } else {
+            alert(`ответ: ${result}`)
+        } 
+    }
 }
+let statusido = true
+while (statusido)calculator ()
