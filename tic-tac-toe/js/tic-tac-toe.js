c5ff02bff28c08dbdc1ab720a84e89b8df2e90f8
chiz0oo <zokyoudzokyoud@gmail.com>
2025-05-11 15:21:58 +0300

    pipy

--- tic-tac-toe/js/tic-tac-toe.js
+++ tic-tac-toe/js/tic-tac-toe.js
@@ -1,85 +1,147 @@
-const getELment = (selector) => {
+const getElement = (selector) => {
     return document.querySelector(selector)
 }
 
-//const startGame = () => {
-//    console.log("Start")
-//}
+class Cell {
+    eliment = null
+    className = 'tic-tac-toe__cell'
+    activeClassName = 'tic-tac-toe__cell--empty'
+    handler = null
+    handlerContext = null
+    handlerArgs = []
+    click = null
 
-//const stopGame = () => {
-//    console.log("Stop")
-//}
+    constructor() {
+        this.click = this._click.bind(this)
+    }
 
-//const switchPlayer = (firstPlayer = true) => {
-//    console.log("Plater switched")
-//}
+    _click() {
+        if (this.handler === null) return
+        if (this.handlerContext === null) {
+            this.handler(...this.handlerArgs)
+        }
+            else this.handler.call(
+                this.handlerContext,
+                ...this.handlerArgs
+            )
+    }
+
+    setHandler(newHandler) {
+        if (typeof newHandler !== 'function')
+        this.handler = newHandler
+        if (typeof context === 'object') this.handlerContext = context
+        else this.handlerContext = null
+        this.handlerArgs = args
+    }
+    create() {
+        this.eliment = document.createElement('div')
+        this.eliment.classList.add(this.className)
+    }
+
+    activate() {
+        if (this.eliment === null) return
+        if(this.eliment.classList.contains(this.activeClassName))
+        //добавляем слушатель события клик
+        this.element.addEventListener('click', this.click)
+        //добавляем класс
+        this.eliment.classList.add(this.activeClassName)
+    }
+
+    deactivate() {
+        if (this.eliment === null) return
+        if(!this.eliment.classList.contains(this.activeClassName))
+        //удаляем слушатель события клик
+        document.removeEventListener('click', this.click)
+        //удоляем класс
+        this.eliment.classList.remove(this.activeClassName)
+
+    }
+
+    fill(content) {
+        if (this.eliment === null) return
+        if (typeof content !== 'string') return
+        this.eliment.innerHtl = content
+    }
+
+    free(content) {
+        if (this.eliment === null) return
+        this.eliment.innerHtl = ''
+    }
+
+    publish(container) {
+        if (this.eliment === null) return
+        if (container instanceof HTMLElement)
+            container.append(this.element)
+    }
+
+}
 
 // cell = {
-//     element: DOMELement
+//     element: DOMElement
 // }
 
-const Field = {
-    
-    size: 3,
+const field = {
+    containerEl: getElement('#tic-tac-toe'),
     cells: [[], [], [],],
 
-    setListeners()  {
+    setListeners() {
         console.log('Listeners')
     },
-
-    fill()  {
-        console.log('fill')
+    fill() {
+        console.log('Fill')
     },
-
-    reset()  {
+    reset() {
         console.log('Reset')
-    }
+    },
+    updateDOM() {
+        console.log('Update')
+    },
 }
 
-const Game = {
-    containerEL: getELment('#tic-tac-toe'),
-    buttonEL: getELment('#tic-tac-toe_btn'),
+const game = {
+    buttonEl: getElement('#tic-tac-toe__btn'),
     isActive: false,
-    field: Field,
+    field: field,
     activePlayer: 0,
     players: [
-    {
-        img: '<svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5 c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4 C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z"></path> </g></svg>'    
-    }, 
-    {
-        img: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>'
-    }],
-    
+        {
+            name: 'Cross',
+            field: [],
+            img: '<svg viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cross</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-467.000000, -1039.000000)" fill="#000000"> <path d="M489.396,1061.4 C488.614,1062.18 487.347,1062.18 486.564,1061.4 L479.484,1054.32 L472.404,1061.4 C471.622,1062.18 470.354,1062.18 469.572,1061.4 C468.79,1060.61 468.79,1059.35 469.572,1058.56 L476.652,1051.48 L469.572,1044.4 C468.79,1043.62 468.79,1042.35 469.572,1041.57 C470.354,1040.79 471.622,1040.79 472.404,1041.57 L479.484,1048.65 L486.564,1041.57 C487.347,1040.79 488.614,1040.79 489.396,1041.57 C490.179,1042.35 490.179,1043.62 489.396,1044.4 L482.316,1051.48 L489.396,1058.56 C490.179,1059.35 490.179,1060.61 489.396,1061.4 L489.396,1061.4 Z M485.148,1051.48 L490.813,1045.82 C492.376,1044.26 492.376,1041.72 490.813,1040.16 C489.248,1038.59 486.712,1038.59 485.148,1040.16 L479.484,1045.82 L473.82,1040.16 C472.257,1038.59 469.721,1038.59 468.156,1040.16 C466.593,1041.72 466.593,1044.26 468.156,1045.82 L473.82,1051.48 L468.156,1057.15 C466.593,1058.71 466.593,1061.25 468.156,1062.81 C469.721,1064.38 472.257,1064.38 473.82,1062.81 L479.484,1057.15 L485.148,1062.81 C486.712,1064.38 489.248,1064.38 490.813,1062.81 C492.376,1061.25 492.376,1058.71 490.813,1057.15 L485.148,1051.48 L485.148,1051.48 Z" id="cross" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>',
+        },
+        {
+            name: 'Zero',
+            field: [],
+            img: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>',
+        }
+    ],
+
     startGame() {
         console.log("Start")
-        console.log(this)
         this.isActive = true
     },
-    
     stopGame() {
         console.log("Stop")
-        this.isActive = stop
+        this.isActive = false
     },
-
     switchPlayer() {
-        console.log("Plater switched")
+        console.log("Player switched")
+        this.activePlayer = (++this.activePlayer) % 2
     },
-    turn(cellEL) {
-        console.log('Players goes')
-    }
+    turn(cellEl) {
+        console.log("Player goes")
+    },
 }
-console.log(Game)
-Game.startGame()
 
-console.log(Game)
-Game.startGame()
+console.log(game)
+game.switchPlayer()
+console.log(game)
+game.switchPlayer()
+console.log(game)
 
-console.log(Game)
-Game.startGame()
-//echo choto
+// echo choto
 
-//utils
 
-//const setListeners = () => {
-//
-//}
+// utils
+
+// const setListeners = () => {}
